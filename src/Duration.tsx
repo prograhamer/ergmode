93ac57dbdf729f612f182951fc33d90cbf15ea77
Graham Brereton <prograhamer@users.noreply.github.com>
2023-11-06 21:11:23 -0500

    fix: support durations over 1h

--- src/Duration.tsx
+++ src/Duration.tsx
@@ -1,10 +1,16 @@
 import DataValue from "./DataValue";
 
 function Duration({ seconds, title }: { seconds: number; title: string }) {
+  const hours = Math.floor(seconds / 3600);
   const minutes = Math.floor((seconds % 3600) / 60);
   const displaySeconds = seconds % 60;
-  const formatted =
-    minutes > 0 ? `${minutes}m ${displaySeconds}s` : `${displaySeconds}s`;
+  let formatted;
+  if (hours > 0) {
+    formatted = `${hours}h ${minutes}m ${displaySeconds}s`;
+  } else {
+    formatted =
+      minutes > 0 ? `${minutes}m ${displaySeconds}s` : `${displaySeconds}s`;
+  }
 
   return <DataValue title={title} value={formatted} />;
 }
